825c6970e27013928a936fc9d2d2adb9cbb2b2be
KamilSulich <44139050+KamilSulich@users.noreply.github.com>
2019-11-15 23:00:46 +0100

    Commit ślepa uliczka, próba dodania rankingu

--- Snake/src/snake/CzytajTablice.java
+++ Snake/src/snake/CzytajTablice.java
@@ -1,11 +1,22 @@
 package snake;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CzytajTablice {
-	 public static int wiersze = 4;
+	
+	File f1=new File("C:\\wynikiWeza.txt"); //Creation of File Descriptor for input file
+    int linecount=0;            //Intializing linecount as zero
+    FileReader fr=new FileReader(f1);  //Creation of File Reader object
+    BufferedReader br = new BufferedReader(fr);    //Creation of File Reader object
+            
+	
+	 public static int wiersze = 2;
 	 public static int Kolumny = 2;
 	 public static String [][] Wyniki = new String[wiersze][Kolumny];
 
--- Snake/src/snake/Ranking.java
+++ Snake/src/snake/Ranking.java
@@ -4,8 +4,10 @@ import javax.swing.JTable;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.IOException;
 
 @SuppressWarnings("serial")
 public class Ranking extends JFrame
@@ -14,11 +16,16 @@ public class Ranking extends JFrame
 	String[] naglowki= {"Gracz","wynik"};
 	String[][] wyniki= {{"Kamil","100"},{"Micha³","47"}};
 
-	
-
 	public Ranking() 
 	{
 		
+		File WynikiWeza = new File("C:\\wynikiWeza.txt");
+		boolean czy_istnieja_statystyki = WynikiWeza.exists();
+		if (!czy_istnieja_statystyki)
+		{
+			File plik = new File("C:\\wynikiWeza.txt");
+		}
+			
 		snake.CzytajTablice czytnik= new snake.CzytajTablice();
 		try {
 			czytnik.CzytajTabliceZpliku();
@@ -27,9 +34,24 @@ public class Ranking extends JFrame
 			e.printStackTrace();
 		}
 		wyniki=CzytajTablice.Wyniki;
+		
+		Arrays.sort(wyniki, new Comparator<String[]>(){
+
+		    @Override
+		    public int compare(final String[] first, final String[] second){
+		        // here you should usually check that first and second
+		        // a) are not null and b) have at least two items
+		        // updated after comments: comparing Double, not Strings
+		        // makes more sense, thanks Bart Kiers
+		        return Double.valueOf(second[1]).compareTo(
+		            Double.valueOf(first[1])
+		        );
+		    }
+		});
+		
 		tabela_wynikow=new JTable(wyniki,naglowki);
 		this.add(tabela_wynikow);
-		setSize(300,200);
+		setSize(500,500);
 		setTitle("Statystyki"); 
 	}
 		
